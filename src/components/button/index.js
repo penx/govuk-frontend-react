2b72ea999030d86690cfa115aa6817121366c29c
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-12-05 19:44:29 +0000

    prop types

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import cx from 'classnames';
 import styles from './_button.module.scss';
 
-const Button = ({ className, ...props }) => (
+// tracks https://github.com/alphagov/govuk-frontend/blob/master/src/components/button/template.njk
+const Button = ({ className, as: Type, ...props }) => (
   <button type="button" className={cx(styles['govuk-button'], className)} {...props} />
 );
+
+Button.propTypes = {
+  // we use as instead of element
+  as: PropTypes.any,
+};
+
+Button.defaultProps = {
+  as: 'button'
+}
 
 export default Button;
