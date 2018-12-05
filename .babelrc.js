e2de9e1451abb31602fbdb60000505efa4305bf3
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-12-05 23:02:08 +0000

    strip babel, add jest, and start and other scripts

--- .babelrc.js
+++ .babelrc.js
@@ -8,24 +8,9 @@ const presets = [
 const plugins =  [
   "@babel/plugin-proposal-object-rest-spread",
   "@babel/plugin-proposal-class-properties",
-  // "@babel/plugin-proposal-decorators",
-  "@babel/plugin-proposal-do-expressions",
   "@babel/plugin-proposal-export-default-from",
-  "@babel/plugin-proposal-export-namespace-from",
-  "@babel/plugin-proposal-function-sent",
-  "@babel/plugin-proposal-json-strings",
-  "@babel/plugin-proposal-logical-assignment-operators",
-  "@babel/plugin-proposal-nullish-coalescing-operator",
-  "@babel/plugin-proposal-numeric-separator",
-  "@babel/plugin-proposal-optional-chaining",
-  // "@babel/plugin-proposal-pipeline-operator",
-  "@babel/plugin-proposal-throw-expressions",
-  "@babel/plugin-syntax-dynamic-import",
-  "@babel/plugin-syntax-import-meta",
-  [
-    "@babel/plugin-transform-runtime",
-    { "helpers": false, /*"polyfill": false,*/ "regenerator": true }
-  ]
+  "@babel/plugin-transform-runtime",
+  "babel-plugin-transform-react-remove-prop-types"
 ];
 
 const ignore = [".spec.js", ".test.js", "-test.js", "/__tests__/"];
