aefe88c5ff414b4fd16e35fc3c9ad229f59690d1
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-12-07 15:42:11 +0000

    react router support in header

--- src/components/header/fixtures.js
+++ src/components/header/fixtures.js
@@ -1,15 +1,37 @@
 import React from 'react';
 // import { action } from '@storybook/addon-actions';
-
+import { MemoryRouter, Link, Switch, Route } from 'react-router-dom';
 import Header from '.';
 
 export default <Header productName="Product" serviceName="Service" navigation="Navigation" serviceUrl="ServiceUrl" />;
 export const withNavigation = (
-  <Header navigation={(
-    <Header.Navigation>
-      <Header.NavigationItem>Item 1</Header.NavigationItem>
-      <Header.NavigationItem>Item 2</Header.NavigationItem>
-    </Header.Navigation>
+  <Header
+    homepage={{ href: '/' }}
+    navigation={(
+      <Header.Navigation>
+        <Header.NavigationItem href="/">Home</Header.NavigationItem>
+        <Header.NavigationItem href="/about">About</Header.NavigationItem>
+      </Header.Navigation>
     )}
   />
+);
+export const withReactRouter = (
+  <MemoryRouter>
+    <React.Fragment>
+      <Header
+        homepage={{ to: '/', as: Link }}
+        navigation={(
+          <Header.Navigation>
+            <Header.NavigationItem as={Link} to="/">Home</Header.NavigationItem>
+            <Header.NavigationItem as={Link} to="/about">About</Header.NavigationItem>
+          </Header.Navigation>
+        )}
+      />
+      <Switch>
+        <Route exact path="/" render={() => 'Home'} />
+        <Route path="/about" render={() => 'About'} />
+        <Route render={() => '404'} />
+      </Switch>
+    </React.Fragment>
+  </MemoryRouter>
 );
--- src/components/header/stories.js
+++ src/components/header/stories.js
@@ -1,6 +1,7 @@
 import { storiesOf } from '@storybook/react';
-import fixture, { withNavigation } from './fixtures';
+import fixture, { withNavigation, withReactRouter } from './fixtures';
 
 storiesOf('Header', module)
   .add('default', () => fixture)
-  .add('with navigation', () => withNavigation);
+  .add('with navigation', () => withNavigation)
+  .add('with react router', () => withReactRouter);
