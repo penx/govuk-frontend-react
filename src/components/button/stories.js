e65bbb7dac0312b4ef6c0f6071906f5a6e923d11
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-12-08 16:11:33 +0000

    eslint

--- src/components/button/stories.js
+++ src/components/button/stories.js
@@ -1,5 +1,7 @@
 import { storiesOf } from '@storybook/react';
-import { withText, asALink, withSomeEmoji, withReactRouter } from './fixtures';
+import {
+  withText, asALink, withSomeEmoji, withReactRouter,
+} from './fixtures';
 
 storiesOf('Button', module)
   .add('with text', () => withText)
