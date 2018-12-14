bd82a7958670cfd6435bb54101ce2f1c52ce0751
Alasdair McLeay <penx@users.noreply.github.com>
2018-12-14 19:15:22 +0000

    code coverage/react testing library (#4)
    * code coverage on button
    
    * 100% code coverage via react-testing-library
    
    * npm install needed on CI

--- src/components/button/test.js
+++ src/components/button/test.js
@@ -1,8 +1,28 @@
 import ReactDOM from 'react-dom';
-import defaultFixture from './fixtures';
+import {
+  render,
+} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import defaultFixture, { hrefAndRole, buttonWithType } from './fixtures';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(defaultFixture, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('hrefAndRole', () => {
+  const { getByText } = render(hrefAndRole);
+  const button = getByText('HREF and role');
+  expect(button).toHaveAttribute('href', '#test');
+  expect(button).toHaveAttribute('role', 'presentation');
+  expect(button.tagName).toBe('A');
+});
+
+it('buttonWithType', () => {
+  const { getByText } = render(buttonWithType);
+  const button = getByText('Button with type');
+  expect(button).toHaveAttribute('type', 'button');
+  expect(button.tagName).toBe('BUTTON');
 });
--- src/components/header/test.js
+++ src/components/header/test.js
@@ -1,9 +1,26 @@
-// TODO: selecting a nav item should close the nav
 import ReactDOM from 'react-dom';
-import defaultFixture from './fixtures';
+import {
+  render,
+  fireEvent,
+} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import defaultFixture, { withNavigation } from './fixtures';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(defaultFixture, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('opens on click', () => {
+  const { getByText, getByLabelText } = render(withNavigation);
+  const menu = getByText('Menu');
+  const tln = getByLabelText('Top Level Navigation');
+  fireEvent.click(menu);
+
+  expect(menu).toHaveClass('govuk-header__menu-button--open');
+  expect(tln).toHaveClass('govuk-header__navigation--open');
+});
+
+// TODO: selecting a nav item should close the nav
