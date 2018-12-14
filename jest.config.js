f7b0bc7ec1136fa3c39e564ca169a83bf44d5180
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-12-14 17:29:13 +0000

    collectCoverageFrom

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   coverageDirectory: '../coverage/',
   collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/**/**.js',
+    '!<rootDir>/**/{stories,example,fixtures}.js',
+    '!<rootDir>/stories/**',
+  ],
   rootDir: 'src',
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
