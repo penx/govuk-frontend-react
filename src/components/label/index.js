9fb016a9ab5226e523679450bdaac785a1eeef8c
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-01-14 15:44:42 +0000

    TODO on label

--- src/components/label/index.js
+++ src/components/label/index.js
@@ -4,6 +4,7 @@ import cx from 'classnames';
 import styles from './_label.module.scss';
 
 const LabelInner = ({ className, for: _for, ...props }) =>
+  // TODO: review, this seems counter intuititive but is required for aligning unit tests
   // eslint-disable-next-line
   (props.children || props.dangerouslySetInnerHTML) ? <label className={cx(styles['govuk-label'], className)} htmlFor={_for} {...props} /> : null
 
