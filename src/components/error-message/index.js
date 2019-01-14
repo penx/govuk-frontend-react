5f5eac3f72225edec8d5616009649fc6ac477596
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-01-14 14:33:28 +0000

    minor prop reorder to match snapshots

--- src/components/error-message/index.js
+++ src/components/error-message/index.js
@@ -4,5 +4,5 @@ import cx from 'classnames';
 import styles from './_error-message.module.scss';
 
 export default ({ className, ...props }) => (
-  <span className={cx(styles['govuk-error-message'], className)} {...props} />
+  <span {...props} className={cx(styles['govuk-error-message'], className)} />
 );
--- src/components/hint/index.js
+++ src/components/hint/index.js
@@ -4,5 +4,5 @@ import cx from 'classnames';
 import styles from './_hint.module.scss';
 
 export default ({ className, ...props }) => (
-  <span className={cx(styles['govuk-hint'], className)} {...props} />
+  <span {...props} className={cx(styles['govuk-hint'], className)} />
 );
