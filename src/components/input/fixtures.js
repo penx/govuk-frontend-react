50404d731630084d6241adad69d15bf43f7112af
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-01-14 19:18:55 +0000

    Final form input example in storybook

--- src/components/input/fixtures.js
+++ src/components/input/fixtures.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Form, Field } from 'react-final-form';
+
 import Input from '.';
+import Button from '../button';
 
 export default (
   <Input
@@ -7,5 +10,30 @@ export default (
     hint={{ children: 'Hint' }}
     errorMessage={{ children: 'Error' }}
     id="test"
+  />
+);
+
+const required = value => (value ? undefined : 'Required');
+
+export const finalForm = (
+  <Form
+    onSubmit={() => null}
+    render={({ handleSubmit, values }) => (
+      <form onSubmit={handleSubmit}>
+        <Field name="firstName" validate={required}>
+          {({ input, meta }) => (
+            <Input
+              type="text"
+              placeholder="First Name"
+              label="First Name"
+              errorMessage={meta.touched && meta.error}
+              {...input}
+            />
+          )}
+        </Field>
+        <Button type="submit">Submit</Button>
+        <pre>Values: {JSON.stringify(values, 0, 2)}</pre>
+      </form>
+    )}
   />
 );
--- src/components/input/stories.js
+++ src/components/input/stories.js
@@ -1,10 +1,11 @@
 import { storiesOf } from '@storybook/react';
 import template from 'govuk-frontend-template-spec/components/input/input.json';
-import fixture from './fixtures';
+import fixture, { finalForm } from './fixtures';
 import render from '../../../tests/render';
 
 const stories = storiesOf('Input', module);
 stories.add('default', () => fixture);
+stories.add('final form', () => finalForm);
 
 template.examples.forEach(example => {
   stories.add(example.name, () => render('input', example.data, { renderMode: 'react' }));
