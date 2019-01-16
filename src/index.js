0189bbb7540b097d6de688367d85b21353a067d1
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-01-16 19:00:32 +0000

    add exports for new components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 export Button from './components/button';
+export ErrorMessage from './components/error-message';
 export Header from './components/header';
+export Hint from './components/hint';
+export Input from './components/input';
+export Label from './components/label';
