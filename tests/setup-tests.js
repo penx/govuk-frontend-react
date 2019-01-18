b6e4a81a74aeedc82b14bf4df4a1dfbef01e8028
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-01-18 14:14:05 +0000

    add TODO on setRender

--- tests/setup-tests.js
+++ tests/setup-tests.js
@@ -1,4 +1,5 @@
 const { toHaveNoViolations } = require('jest-axe');
+// TODO: setRender should be exported from govuk-frontend-template-spec root
 const { setRender } = require('govuk-frontend-template-spec/lib/jest-helpers');
 const render = require('./render');
 
