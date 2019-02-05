46421bfbde0201026c119adf35f03ea4a5307a6f
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-02-05 23:48:25 +0000

    final form story for radios

--- src/components/radios/stories.js
+++ src/components/radios/stories.js
@@ -1,5 +1,11 @@
+import * as React from 'react';
+import { Form, Field } from 'react-final-form';
 import { storiesOf } from '@storybook/react';
 import template from 'govuk-frontend-template-spec/components/radios/radios.json';
+import { action } from '@storybook/addon-actions';
+
+import Radios from '.';
+import Button from '../button';
 import render from '../../../tests/render';
 
 const stories = storiesOf('Radios', module);
@@ -25,3 +31,30 @@ stories.add('testing', () =>
 template.examples.forEach(example => {
   stories.add(example.name, () => render('radios', example.data, { renderMode: 'react' }));
 });
+
+const FFRadio = ({ input, meta, children }) => <Radios.Radio {...input}>{children}</Radios.Radio>;
+
+// TODO: validation required item, error message display
+// TODO: name=colour should be accessible from context api
+stories.add('with final form, individual fields', () => (
+  <Form
+    initialValues={{ colour: '' }}
+    onSubmit={action('submit')}
+    render={({ handleSubmit, values }) => (
+      <form onSubmit={handleSubmit}>
+        <Radios name="colour">
+          <Field type="radio" name="colour" value="blue" component={FFRadio}>
+            Blue
+          </Field>
+          <Field type="radio" name="colour" value="red" component={FFRadio}>
+            Red
+          </Field>
+        </Radios>
+        <Button type="submit">Submit</Button>
+        <pre>Values: {JSON.stringify(values, 0, 2)}</pre>
+      </form>
+    )}
+  />
+));
+
+// TODO: final form with Radios as single field
