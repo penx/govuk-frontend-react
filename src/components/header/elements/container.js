a01c640ab21f2c982237de2d212ea432eefcfdd9
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-02-28 19:24:56 +0000

    add WidthContainer component

--- src/components/header/elements/container.js
+++ src/components/header/elements/container.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import cx from 'classnames';
 import styles from '../_header.module.scss';
+import WidthContainer from '../../../objects/width-container';
 
-const Container = ({ className, ...props }) => (
-  <div className={cx(styles['govuk-header__container'], className)} {...props} />
-);
+const Container = ({ className, ...props }) =>
+  className ? (
+    <div className={cx(styles['govuk-header__container'], className)} {...props} />
+  ) : (
+    <WidthContainer className={styles['govuk-header__container']} {...props} />
+  );
 
 Container.defaultProps = {
-  className: styles['govuk-width-container']
+  className: undefined
 };
 
 export default Container;
