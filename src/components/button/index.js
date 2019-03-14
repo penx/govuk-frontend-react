ad7bd1523fe81f9e2d2617c8965b0946f68b8c56
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-03-14 12:11:53 +0000

    only export css module version of button

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,4 +1,7 @@
 export default from './module';
-export css from './css';
-export module from './module';
-export template from './template';
+
+// We're not exporting different types yet, until we have ensured a working system for all components.
+// We need to ensure that if we export css.js from here then it gets picked up by tree shaking if not used.
+// export css from './css';
+// export module from './module';
+// export template from './template';
