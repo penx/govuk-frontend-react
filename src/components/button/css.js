8ed59ed53e1624c62be2c90c7d048a9b9e6d4dc4
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-03-14 12:20:06 +0000

    we don't need css.js for now

--- src/components/button/css.js
+++
@@ -1,6 +0,0 @@
-// This version of button ensures CSS has been imported (not as a CSS module) and uses default CSS classes
-import React from 'react';
-import 'govuk-frontend/components/button/_button.scss';
-import Template from './template';
-
-export default props => <Template {...props} />;
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,7 +1,2 @@
 export default from './module';
-
-// We're not exporting different types yet, until we have ensured a working system for all components.
-// We need to ensure that if we export css.js from here then it gets picked up by tree shaking if not used.
-// export css from './css';
-// export module from './module';
-// export template from './template';
+export template from './template';
