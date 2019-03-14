927cce362b1513ed9ca86945bfea90460c71e5ce
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-03-14 12:06:57 +0000

    remove templates index

--- src/templates.js
+++
@@ -1,9 +0,0 @@
-export Button from './components/button/template';
-// export DateInput from './components/date-input/template';
-// export ErrorMessage from './components/error-message/template';
-// export Fieldset from './components/fieldset/template';
-// export Header from './components/header/template';
-// export Hint from './components/hint/template';
-// export Input from './components/input/template';
-// export Label from './components/label/template';
-// export Radios from './components/radios/template';
