5d06096dc3d1e152d3c5332107b8d184bdb8e965
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-03-14 19:24:41 +0000

    form group and width container templates

--- src/objects/form-group/template.js
+++ src/objects/form-group/template.js
@@ -3,15 +3,32 @@
 import * as React from 'react';
 import cx from 'classnames';
 
-const FormGroup = ({ className, error, ...props }: React.ElementProps<'div'>) => (
-  <div
-    className={cx(
-      styles['govuk-form-group'],
-      error && styles['govuk-form-group--error'],
-      className
-    )}
-    {...props}
-  />
-);
+export type ClassNames = {|
+  'govuk-form-group': string,
+  'govuk-form-group--error': string
+|};
+
+const defaultClassNames: ClassNames = {
+  'govuk-form-group': 'govuk-form-group',
+  'govuk-form-group--error': 'govuk-form-group--error'
+};
+
+const FormGroup = ({ className, classNames, error, ...props }: React.ElementProps<'div'>) => {
+  const styles: ClassNames = {
+    ...defaultClassNames,
+    ...classNames
+  };
+
+  return (
+    <div
+      className={cx(
+        styles['govuk-form-group'],
+        error && styles['govuk-form-group--error'],
+        className
+      )}
+      {...props}
+    />
+  );
+};
 
 export default FormGroup;
--- src/objects/width-container/template.js
+++ src/objects/width-container/template.js
@@ -3,8 +3,21 @@
 import * as React from 'react';
 import cx from 'classnames';
 
-const WidthContainer = ({ className, ...props }: React.ElementProps<'div'>) => (
-  <div className={cx(styles['govuk-width-container'], className)} {...props} />
-);
+export type ClassNames = {|
+  'govuk-width-container': string
+|};
+
+const defaultClassNames: ClassNames = {
+  'govuk-width-container': 'govuk-width-container'
+};
+
+const WidthContainer = ({ className, classNames, ...props }: React.ElementProps<'div'>) => {
+  const styles: ClassNames = {
+    ...defaultClassNames,
+    ...classNames
+  };
+
+  return <div className={cx(styles['govuk-width-container'], className)} {...props} />;
+};
 
 export default WidthContainer;
