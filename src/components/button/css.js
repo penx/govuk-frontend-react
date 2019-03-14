47eabd27c49c24a92f241488ca0cef94166f0074
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-03-14 12:11:42 +0000

    comments about button versions

--- src/components/button/css.js
+++ src/components/button/css.js
@@ -1,3 +1,4 @@
+// This version of button ensures CSS has been imported (not as a CSS module) and uses default CSS classes
 import React from 'react';
 import 'govuk-frontend/components/button/_button.scss';
 import Template from './template';
--- src/components/button/module.js
+++ src/components/button/module.js
@@ -1,3 +1,4 @@
+// This version of button imports styles as a CSS module and provides custom classNames to the template
 import React from 'react';
 import styles from './styles.module.scss';
 import Template from './template';
